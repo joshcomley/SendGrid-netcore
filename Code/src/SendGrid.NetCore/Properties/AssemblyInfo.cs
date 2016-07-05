5dbbdab28cb603bf87a0f10afd62b43dd4f23e65
Josh Comley <joshcomley@googlemail.com>
2016-07-05 20:03:10 +0200

    Updated to RTM

--- Code/src/SendGrid.NetCore/Properties/AssemblyInfo.cs
+++ Code/src/SendGrid.NetCore/Properties/AssemblyInfo.cs
@@ -20,4 +20,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("52e32ce0-16af-4e54-b9ba-b31660a00f3b")]
+//[assembly: Guid("52e32ce0-16af-4e54-b9ba-b31660a00f3b")]
